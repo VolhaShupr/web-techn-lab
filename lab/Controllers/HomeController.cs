d5378e9609ba3c7c6aff9fabaab24d2ee9f004bf
Volha Shupranava <volha_shupr@tut.by>
2020-09-16 16:31:27 +0300

    Test commit

---
+++ lab/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using lab.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class ListDemo
+        {
+            public int ListItemValue { get; set; }
+            public string ListItemText { get; set; }
+        }
+
+        private List<ListDemo> _listDemo;
+
+        public HomeController()
+        {
+            _listDemo = new List<ListDemo>
+            {
+            new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+            new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+            new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
+        }
+
+
+        [ViewData]
+        public string Text { get; set; }
+        [ViewData]
+        public SelectList Lst { get; set; }
+        public IActionResult Index()
+        {
+            Text = "Lab 2";
+            Lst = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
