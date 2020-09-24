cb5b6311a43153d9df5ab888dcf115658a478fe0
Volha Shupranava <volha_shupr@tut.by>
2020-09-24 16:59:05 +0300

    [lab10] Add Blazor project

---
+++ lab.Blazor/Data/ApiViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace lab.Blazor.Data
+{
+    public class MusInstrumentListViewModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+    }
+
+    public class MusInstrumentDetailsViewModel
+    {
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } 
+        
+        [JsonPropertyName("description")]
+        public string Description { get; set; } 
+        
+        [JsonPropertyName("price")]
+        public double Price { get; set; } 
+        
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+    }
+}
---
+++ lab.Blazor/Data/CounterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab.Blazor.Data
+{
+    public class CounterModel
+    {
+        [DataType("int")]
+        [Range(0, int.MaxValue)]
+        public int CounterValue { get; set; }
+    }
+}
